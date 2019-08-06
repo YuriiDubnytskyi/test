e049aa1fb410b9d8c9fd878313201f01664a7eb9
Yurii Dubnytskyi <41127730+YuriiDubnytskyi@users.noreply.github.com>
2019-08-06 20:06:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,7 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
-
-
-const ServerPortRouter = express.Router();
 const mongoose = require('mongoose');
-
-const Schema = mongoose.Schema;
-const ServerPort = new Schema({
-  name: {
-    type: String
-  },
-  port: {
-      type: Number
-  }
-},{
-    collection: 'servers'
-});
 
 mongoose.connect('mongodb+srv://yuriy:Wdj_7yex6cE5cjp@cluster0-odkqs.mongodb.net/test?retryWrites=true&w=majority',{useNewUrlParser:true}).then(
     () => {console.log('Database is connected') },
@@ -26,40 +10,11 @@ mongoose.connect('mongodb+srv://yuriy:Wdj_7yex6cE5cjp@cluster0-odkqs.mongodb.net
 
 
 
-ServerPortRouter.route('/add').post(function (req, res) {
-  const serverport = new ServerPort(req.body);
-  serverport.save()
-    .then(serverport => {
-        res.json('Server added successfully');
-    })
-    .catch(err => {
-    res.status(400).send("unable to save to database");
-    });
-});
-
-ServerPortRouter.route('/').get(function (req, res) {
-    ServerPort.find(function (err, serverports){
-    if(err){
-      console.log(err);
-    }
-    else {
-      res.json(serverports);
-    }
-  });
-});
-
 app.use('/serverport', ServerPortRouter);
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 // Put all API endpoints under '/api'
-
-
-
-
-
-
-
 
 
 // The "catchall" handler: for any request that doesn't
