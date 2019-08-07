8702db5f8c39b41bdbb1543464bfc0fa3b6f17b0
Yurii Dubnytskyi <41127730+YuriiDubnytskyi@users.noreply.github.com>
2019-08-07 22:00:49 +0300

    Update Create.js

--- client/src/components/Create.js
+++ client/src/components/Create.js
@@ -2,45 +2,65 @@ import React, { Component } from 'react';
 import axios from 'axios';
 export default class Create extends Component {
 
+
+
     constructor(props) {
         super(props);
-        this.onChangeHostName = this.onChangeHostName.bind(this);
-        this.onChangePort = this.onChangePort.bind(this);
         this.onSubmit = this.onSubmit.bind(this);
-
+        this.onChangeName = this.onChangeName.bind(this);
+        this.onChangeIngredients = this.onChangeIngredients.bind(this);
+        this.onChangePreparation = this.onChangePreparation.bind(this);
+        this.onChangeAuthor = this.onChangeAuthor.bind(this);
         this.state = {
             name: '',
-            port: ''
+            ingredients: '',
+            preparation: '',
+            author: ''
+
         }
     }
-    onChangeHostName(e) {
+    onChangeName(e) {
         this.setState({
             name: e.target.value
         });
     }
-    onChangePort(e) {
+    onChangeIngredients(e) {
         this.setState({
-            port: e.target.value
+            ingredients: e.target.value
+        });
+    }
+    onChangePreparation(e) {
+        this.setState({
+            preparation: e.target.value
+        });
+    }
+    onChangeAuthor(e) {
+        this.setState({
+            author: e.target.value
         });
     }
     onSubmit(e) {
         e.preventDefault();
         const serverport = {
             name: this.state.name,
-            port: this.state.port
+            ingredients: this.state.ingredients,
+            preparation:this.state.preparation,
+            author:this.state.author
         }
         axios.post('/serverport/add', serverport)
         .then(res => console.log(res.data));
         
         this.setState({
             name: '',
-            port: ''
+            ingredients: '',
+            preparation: '',
+            author: ''
         });
     }
 
     render() {
         return (
-            <div style={{marginTop: 50}}>
+            /*<div style={{marginTop: 50}}>
                 <h3>Add New Server</h3>
                 <form onSubmit={this.onSubmit}>
                     <div className="form-group">
@@ -55,7 +75,27 @@ export default class Create extends Component {
                         <input type="submit" value="Add Node server" className="btn btn-primary"/>
                     </div>
                 </form>
+            </div>*/
+            <div className="form-group">
+                <form onSubmit={this.onSubmit}>
+                    <label>Hot Dog name</label>
+                    <input type="text" className="form-control" value={this.state.name} onChange={this.onChangeName}/>
+                    <label>Ingredients</label>
+                    <textarea className="form-control" rows="6" value={this.state.ingredients} onChange={this.onChangeIngredients}></textarea>
+                    <label>Preparation</label>
+                    <textarea className="form-control" rows="6" value={this.state.preparation} onChange={this.onChangePreparation}></textarea>
+                    <label>Author</label><input
+                    type="text" className="form-control" value={this.state.author} onChange={this.onChangeAuthor}/>
+        
+                    <input type="submit" value="Add" className="btn btn-secondary m-2"/>
+                </form>
             </div>
+
+
+
+
+
+
         )
     }
 }
